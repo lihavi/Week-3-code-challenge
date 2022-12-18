68919884a914d2db28390a34067af71e0864bbc4
vivianne <viviannjoroge91@gmail.com>
2022-12-18 09:08:05 +0300

    Completed assignment

--- script.js
+++ script.js
@@ -1,85 +1,93 @@
-document.addEventListener("DOMContentLoaded", () =>{
-  getfilms(films)
-})  
+const filmTime = document.getElementById('film-item')
+//declare the json url as an API for easy access.
+const API = "http://localhost:3000/films";
 
-const movies = document.getElementById("movies");
-const head = document.createElement("h2");
-movies.appendChild(head);
-
-head.textContent = "Click on your favourite movie!";
-
-const title = document.getElementById("title");
-const runTime = document.getElementById("runTime");
-const capacity = document.getElementById("capacity");
-const showTime = document.getElementById("showtime");
-let tickets = document.getElementById("tickets");
-const description = document.getElementById("description");
-const form = document.querySelector("#form")
-const input = document.createElement("input")
-input.type = "hidden"
-input.className = "inputs"
-form.appendChild(input)
-fetch("http://localhost:3000/films")
+fetch(API)
 .then((res) => res.json())
-.then (function(films){
-    console.log(films);
-    films.map(function (films) {
-        const list = document.createElement("ul");
-        movies.appendChild(list);
-        const movieList = document.createElement("li");
-  
-        list.appendChild(movieList);
-  
-        movieList.innerHTML = `${films.title}`;
-  
-        movieList.addEventListener("click", showMovie);
-  
-        function showMovie(e) {
-          e.preventDefault();
-          const images = document.getElementById("images");
-  
-          images.src = films.poster;
-          title.textContent = `Title: ${films.title}`;
-          runTime.textContent = `Runtime: ${films.runtime}`;
-          capacity.textContent = `capacity: ${films.capacity}`;
-          showTime.textContent = `Showtime: ${films.showtime}`;
-          tickets.textContent = films.tickets;
-          description.textContent = `Description: ${films.description}`;
-          
-        }
-  
-      });
-  
-    });
-  
-  
-    const btn = document.getElementById("btn")
-  
-    btn.addEventListener("click", function(e){
+.then(renderFilm) 
+ //const allMovies = renderFilm;
+
+
+//rendering only the one poster and its details.
+function renderFilm(film) {
+    const filmMenuDiv = document.getElementById("film-menu");
+    const titleDiv = document.getElementById("film-details"); 
+    const runDiv = document.getElementById("runtime")
+    const showDiv = document.getElementById("showtime")
+    const availableDiv = document.getElementById("Available-tickets")
+
+    filmMenuDiv.src = film.poster;
+
+    const filmTitle = document.createElement("p");
+    filmTitle.textContent = film.title;
+    titleDiv.append(filmTitle);
+
+    const filmRuntime = document.createElement("p");
+    filmRuntime.textContent = film.runtime;
+    runDiv.append(filmRuntime);
+
+    const filmShowtime = document.createElement("p");
+    filmShowtime.textContent = film.showtime;
+    showDiv.append(filmShowtime);
+
+    const filmAvailability = document.createElement("p");
+    filmAvailability.textContent = (film.capacity - film.tickets_sold);
+    availableDiv.append(filmAvailability);
+     
+    //for the buying of tickets button 
+    const button = document.getElementById("sell");
+    const btn = parseInt(availableDiv.innerText);
+    console.log(btn);
+    button.addEventListener('click', () => {
+       if (btn >= 1) {
+        availableDiv.innerText = (availableDiv.innerText - 1)
+        
+       }
+      
+       if (availableDiv.innerText < 0) {
+        availableDiv.innerText = '0';
+        alert ('Sorry, we are currently sold out of tickets for this film.')
+       }
+
+       /*if (availableDiv.innerHTML <= 0) {
+        button.innerHTML = 'SOLD OUT'
+       }*/
+
     
-      e.preventDefault()
-    
-    
-        let noOfTickets = document.getElementById("amount").value
-        const id = document.querySelector(".inputs").id
-    
-    
-        const newTickets = parseInt( tickets.textContent - noOfTickets)
-    
-        tickets.textContent = newTickets
-  
-        fetch(`http://localhost:3000/films${id}`,{
-          method: "POST",
-  
-        headers:{
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(films.tickets)
-        })
-        .then(res => res.json())
-        .then(function(films){
-          films.tickets = newTickets
-  
-        })
     })
-  
+
+}
+//a second fetch request to get the movie list.
+function getFilms(){
+    fetch("http://localhost:3000/films")
+    .then((response) => response.json())
+    .then(renderFilms);
+}
+getFilms();
+
+function renderFilms(films) {
+    films.forEach(filmDetails);
+}
+
+function filmDetails(details) {
+    const titlesElement = document.getElementById("film-item");
+
+    let listElement = document.createElement("li");
+    listElement.innerText = details.title;
+    listElement.className = 'film-item'
+    
+    let imgElement = document.createElement("film-details");
+    imgElement.src = details.poster;
+
+  const moviePoster = document.getElementById('film-menu')
+
+    titlesElement.append(listElement);
+    listElement.addEventListener('click', () => {
+        console.log(details.title)
+        document.getElementById('film-details').innerText = details.title
+        document.getElementById('runtime').innerText = details.runtime
+        document.getElementById('showtime').innerText = details.showtime
+        document.getElementById('Available-tickets').innerText = details.capacity - details.tickets_sold
+        moviePoster.src = details.poster
+    })
+}
